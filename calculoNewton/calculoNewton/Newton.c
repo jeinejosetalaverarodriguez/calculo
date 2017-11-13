2ef07b51befc84b2cf0cac05dbff9eb3404c9f97
jeine jose talavera rodriguez <jjtr_91@hotmail.com>
2017-11-13 14:39:25 -0400

    terminado

--- calculoNewton/calculoNewton/Newton.c
+++ calculoNewton/calculoNewton/Newton.c
@@ -22,17 +22,25 @@
 
 
 
-/*Funcion que queremos aproximar*/
+//Funcion que queremos aproximar*/
 double funcion(double x) {
 	
 	return (pow(x,3)-x-1);
 }
 
-/*Derivada de la función a aproximar*/
+//Derivada de la función a aproximar*/
 double dfx(double x) {
 	
 	return ((3*pow(x, 2)) - 1);
 }
+//segunda derivada
+double ddfx(double x) {
+
+	return (6 * x);
+}
+
+
+
 /*Formula iterativa de NewtonRapson*/
 double f(double xi) {
 
@@ -46,29 +54,54 @@ double error(double ant, double act) {
 
 }
 
+int convergencia(double x) {
+	double aux= ((funcion(x)*ddfx(x)) / pow(dfx(x), 2));
+	int s;
+	if (aux<1) {
+		s = 1;
+	printf("\n el valo de Xi=%.2lf cumple con el citerio de convergencia\n",x);
+	printf("\n criterio de convergencia=%lf<1\n", aux);
+	}
+	else {
+		s = 0;
+		printf("valor de Xi=%lf no cumple con el criterio de convergencia", x);
+		printf("\n criterio de convergencia=%lf >  1\n", aux);
+	}
+	printf("\n////////////////////////////////////////////////////////////////////////////////\n");
+	printf("\n");
+	return s;
+}
 
 void Newton_Rapson(double xi, double cota, int n) {/*Algoritmo de Newton-Rapson*/
 	int i = 0;
 	double er = cota + 1, aux = 0;
+	
+	if (convergencia(xi) == 1){
+		do {
+			if (i == 0) {
+				printf("i = %d\t X%d\ = %.6lf\t	 X%d\ = %.6lf\t\n", i, i, xi, i + 1, f(xi));
 
-	do {
-		if (i == 0) {
-			printf("i = %d\t X%d\ = %.6lf\t	 X%d\ = %.6lf\t\n", i, i , xi, i+1, f(xi));
+			}
+			else {
+				printf("i = %d\t X%d\ = %.6lf\t	 X%d\ = %.6lf\t\n", i, i, xi, i + 1, f(xi), er = error(aux, f(xi)));
 
-		}
-		else {
-			printf("i = %d\t X%d\ = %.6lf\t	 X%d\ = %.6lf\t\n", i,  i, xi, i + 1, f(xi), er = error(aux, f(xi)));
-			
-		}
+			}
 
-		aux = f(xi);
-		xi = f(xi);
+			aux = f(xi);
+			xi = f(xi);
 
-		i++;
-	} while (i<n && er>cota);
-
-	printf("funcion = %.11lf", funcion(xi));/*Falta arreglar la comprobacion */
-
+			i++;
+		} while (i<n && er>cota);
+		printf("\n////////////////////////////////////////////////////////////////////////////////\n");
+		printf("\n");
+		printf("X%i=%.11lf\n",i, xi);
+		printf("\nERROR=%.11lf",er = error(aux, f(xi)));
+		printf("\n////////////////////////////////////////////////////////////////////////////////\n");
+		printf("\n");
+		printf("f(X%i) = %.11lf",i, funcion(xi));
+		printf("\n////////////////////////////////////////////////////////////////////////////////\n");
+		printf("\n");
+}
 }
 
 
