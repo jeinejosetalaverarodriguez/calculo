7dfb71611b75fed144d2d1c3751146ed1d5b1533
jeine jose talavera rodriguez <jjtr_91@hotmail.com>
2017-11-21 19:37:15 -0400

    listo(falta error)

--- calculoTrapecio/calculoTrapecio/trapecio.c
+++ calculoTrapecio/calculoTrapecio/trapecio.c
@@ -9,7 +9,7 @@ int main(int argc, char **argv) {
 	int N;
 
 	printf("Metodo de intregacion compuesto del trapecio\n");
-	printf("La funcion a integrar es SENO(X)/(1+X^4)^0.5\n");
+	printf("La funcion a integrar es SENO(X^2)\n");
 	
 	printf("Ingrese el limite inferior de la integral---A: \n");
 	scanf_s("%lf", &a);//limite inferior d la integracion
@@ -24,21 +24,37 @@ int main(int argc, char **argv) {
 	i = 0;
 	suma = 0;
 
+
 	for (i = 0;i <= N;i++) {
-		xi = a + i*h;
-		F = (xi*( sin (pow(xi,2))));
-
-		suma = suma + F;
-		t = suma * h;
+		if (i == 0) {
+			xi = a;
+			F = ((sin(pow(xi, 2))));
+			suma = suma + F;
+		}
+		else {
+			if (i == N) {
+				xi = b;
+				F = ((sin(pow(xi, 2))));
+				suma = suma + F;
+			}
+			else {
+				xi = a + (i*h);
+				F = ((2 * sin(pow(xi, 2))));
+				suma = suma + F;
+			}
+		}
+		
 	}
+	t = suma * (h/2);
+	
 	printf("Los valores son:\n");
-	printf("a = %.3lf\n" ,a );
-	printf("b = %.3lf\n", b );
-	printf("h = %.3lf\n", h );
-	printf("la suma es: %.3lf\n", suma );
+	printf("a = %.4lf\n" ,a );
+	printf("b = %.4lf\n", b );
+	printf("h = %.4lf\n", h );
+	printf("la suma es: %.4lf\n", suma );
 
 
-	printf("LA INTEGRAL ES : %.3lf\n", t );
+	printf("LA INTEGRAL ES : %.4lf\n", t );
 
 	system("PAUSE");
 	return 0;
